341b4e4274e387edf1eccb28f1ce5b1063313908
markohautala <>
2023-10-21 18:53:09 +0000

    JS code to get start button hidden when clicked on

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const startButton = document.getElementById('start-button')
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-
+    console.log('started')
+    startButton.classList.add('hide')
 }
 
 
