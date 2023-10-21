1b5431e5e0ae25345f958d955fc0f60b5e207fc6
markohautala <>
2023-10-21 18:59:46 +0000

    when start quiz button is clicked it removes the hide function which makes the quiz pop-up

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const startButton = document.getElementById('start-button')
+const questionContainerElement = document.getElementById('question-div')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('started')
     startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
 }
 
 
