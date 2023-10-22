6307d44e5adf798c89647d04e253755010ad7433
markohautala <>
2023-10-22 17:35:42 +0000

    doing so that the first child elements are deleted when new set of questions are loaded

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const startButton = document.getElementById('start-button')
+const nextButton = document.getElementById('next-button')
 const questionContainerElement = document.getElementById('question-div')
 
-let randomQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 const questionElement = document.getElementById('question')
-const answerButtonElement = document.getElementById('answer-buttons')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 // when start quiz button is clicked, startGame function is started
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('started')
 
     //this hides the start quiz button
     startButton.classList.add('hide')
     
-    //randomizes the questions
-    randomQuestions = questions.sort(() => Math.random() - .5)
+    //randomizes or shuffles all the questions
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
 
+    // we are starting from index 0 in our shuffled question array
     currentQuestionIndex = 0
 
     // this section removes the hide-class - and then shows the quiz itself 
@@ -29,40 +30,44 @@ function startGame() {
 
 
 function setNextQuestion() {
-    resetState() //this will trigger a function that resets the questions - deletes the old ones
-    showQuestion(randomQuestions[currentQuestionIndex])
+    resetState(); //this will trigger a function that resets the questions - deletes the old ones
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question)  {
     questionElement.innerText  = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement('button')//creates buttons for each answer
-        button.innerText = answers.text
+        const button = document.createElement('button') //creates buttons for each answer
+        button.innerText = answer.text
         button.classList.add('button') //adds the "button"-class to the buttons, so that css styles them
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer) 
-        answerButtonElement.appendChild(button)
+        answerButtonsElement.appendChild(button)
     })
 }
 
 function resetState() {
-    next-Button.classList.add('hide') //this adds the hide class, which makes it hidden by CSS
+    nextButton.classList.add('hide') //this adds the hide class, which makes it hidden by CSS
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
 
 }
 
 const questions = [
     {
-        question: "What is the core of Hermione Granger's wand?",
+        question: 'What is the core of Hermione Granger\'s wand?',
         answers: [
-            { text: 'Dragon Heartstring', correct: true},
-            { text: 'Phoenix Feather', correct: false},
-            { text: 'Veela Hair', correct: false},
-            { text: 'Thestral Tail Hair', correct: false},
+            { text: 'Dragon Heartstring', correct: true },
+            { text: 'Phoenix Feather', correct: false },
+            { text: 'Veela Hair', correct: false },
+            { text: 'Thestral Tail Hair', correct: false }
         ]
     }
-]
+];
