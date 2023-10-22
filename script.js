ea77785f4529b331984c1c9a3ae30b6d4c2575c1
markohautala <>
2023-10-22 18:36:44 +0000

    added nextbutton incrementation that triggers the function that sets the next questions. If there is more questions, then next button is shown. If not, then restart game button is triggered

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const startButton = document.getElementById('start-button')
 const nextButton = document.getElementById('next-button')
 const questionContainerElement = document.getElementById('question-div')
 
-let shuffledQuestions, currentQuestionIndex
-
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+//these ones are set to let because they change - can't be const
+let shuffledQuestions, currentQuestionIndex
+
 // when start quiz button is clicked, startGame function is started
 startButton.addEventListener('click', startGame)
+//when next-button clicked, current question index is incremented and jumps to set next question function
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
 
@@ -55,9 +61,35 @@ function resetState() {
     }
 
 }
-
+//indictes whatever the user clicked on - event trigger
 function selectAnswer(e) {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    //if we have more questions, when next-button is shown
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide'); 
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+//adds classes of either wrong or correct
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+//clears classes from the elements
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
